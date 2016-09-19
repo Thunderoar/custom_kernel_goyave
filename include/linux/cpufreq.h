cfd4e178e7e8a19a2dd6b4e34daa455bb21b3959
Thunderoar <farhansamilme@gmail.com>
2016-09-19 16:22:47 +0800

    Add: Initial intelliactive governor addition

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -403,6 +403,9 @@ extern struct cpufreq_governor cpufreq_gov_sprdemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
 extern struct cpufreq_governor cpufreq_gov_darkness;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
+extern struct cpufreq_governor cpufreq_gov_intelliactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intelliactive)
 #endif
 
 
